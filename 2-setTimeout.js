8e25ef02bfab3e5c255fc7691ca927268361f505
Ravi Kumar Yadav <ravi.yadav@paytmmall.com>
2021-11-22 14:42:10 +0530

    cleaned everything

--- 2-setTimeout.js
+++
@@ -1,5 +0,0 @@
-function print(salutation, name, message) {
-	console.log('Hi ' + salutation, ' ', name, ': ', message);
-}
-
-setTimeout(print, 2 * 1000, 'Mr.', 'Balram', 'You are AWESOME!!!');
--- 2-setTimeoutAdvance.js
+++
@@ -1,9 +0,0 @@
-// print "Hello after 4sec" and "Hello after 8sec" using only one method
-
-const one = function(seconds) {
-	console.log('Hello after ', seconds, 'sec');
-}
-
-
-setTimeout(one, 4 * 1000, 4);
-setTimeout(one, 8 * 1000, 8);
